40f0000aa6e798757cb1c888f660c2ef490e3812
Juan Camilo Davila R <jcamilodavila25@gmail.com>
2025-07-08 17:44:56 -0500

    Actualizando

--- models/task.js
+++ models/task.js
@@ -1,4 +1,20 @@
 import mongoose from 'mongoose';
 
+const taskSchema = new mongoose.Schema({
+    project: { type: mongoose.Schema.ObjectId, Ref: 'Project'},
+    name: String,
+    description: String,
+    status: { type: String, enum: ['alta', 'media', 'baja'], default: 'media'},
+    assignedTo: { type: mongoose.Schema.ObjectId, Ref: 'User'},
+    dueDate: Date,
+    subTasks: [{ type: mongoose.Schema.ObjectId, Ref: 'Task'}],
+    comments: [{
+        user: { type: mongoose.Schema.ObjectId, Ref: 'User'},
+        content: String,
+        createdAt: { type: Date, default: Date.now }
+    }],
+    createdAt: { type: Date, default: Date.now }
+});
 
+export default mongoose.model('Task', taskSchema);
 
